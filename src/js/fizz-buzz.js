d2d0f4ab28d83c91051aecef378aeaf6fd5ecd22
Jonathan <jon1969edwards@gmail.com>
2018-11-09 15:46:07 +0100

    adds test for number 5, all tests green

---
+++ src/js/fizz-buzz.js
@@ -0,0 +1,21 @@
+function FizzBuzz() {
+    this.check = (number) => {
+        if (number % 3 === 0) {
+            return 'Fizz';
+        } else {
+            return number
+        }
+    }
+}
+
+function FizzBuzz() {
+    this.check = (number) => {
+        if (number % 5 === 0) {
+            return 'Buzz';
+        } else if (number % 3 === 0) {
+            return 'Fizz';
+        } else {
+            return number
+        }
+    }
+}

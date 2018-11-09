d279fc2587b6cbe36a763cffa314461adba59a4c
Jonathan <jon1969edwards@gmail.com>
2018-11-09 17:06:18 +0100

    adds test for number 15, all tests run green

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -14,4 +14,8 @@ const { FizzBuzz } = require('./spec.helper')
     it('returns Buzz if number is divisible by 5', () => {
         expect(fizzBuzz.check(5)).to.eql('Buzz')
     })
+
+    it('returns FizzBuzz if number is divisible by 15', () => {
+        expect(fizzBuzz.check(15)).to.eql('FizzBuzz')
+    })
 })
